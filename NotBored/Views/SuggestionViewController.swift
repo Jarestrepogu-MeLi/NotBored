9f2d156ff73450739c1cc6fe8917d24b129afd5a
mmbattaglia <mariano.battaglia@mercadolibre.com>
2022-06-07 10:23:18 -0300

    Creacion de la vista SuggestionViewController

---
+++ NotBored/Views/SuggestionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SuggestionViewController.swift
+//  NotBored
+//
+//  Created by Mariano Martin Battaglia on 07/06/2022.
+//
+
+import UIKit
+
+class SuggestionViewController: UIViewController {
+
+    @IBOutlet weak var participantsLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onTapTryAnother(_ sender: Any) {
+    }
+    
+    
+}
