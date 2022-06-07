25da020bf49e046058f16aac02e89debac1fe90c
Jorge Restrepo <jorge.restrepo@mercadolibre.com.co>
2022-06-07 14:57:23 -0500

    Terms and Conditions view y view controller.

---
+++ NotBored/Utils/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  NotBored
+//
+//  Created by Jorge Andres Restrepo Gutierrez on 7/06/22.
+//
+
+import UIKit
+
+
+enum categories: String {
+    case education = "Education",
+         recreational = "Recreational",
+         social = "Social",
+         diy = "Diy",
+         charity = "Charity",
+         cooking = "Cooking",
+         relaxation = "Relaxation",
+         music = "Music",
+         busywork = "Busywork"
+}
+
+struct nBColors {
+    static let backgroundColor = UIColor(red: 0.875, green: 0.952, blue: 0.992, alpha: 1.0)
+    static let titleColor = UIColor(red: 0.255, green: 0.561, blue: 0.886, alpha: 1.0)
+    static let bodyColor = UIColor(red: 0.083, green: 0.065, blue: 0.234, alpha: 1.0)
+}
---
+++ NotBored/Views/TermsConditionsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TermsConditionsViewController.swift
+//  NotBored
+//
+//  Created by Jorge Andres Restrepo Gutierrez on 7/06/22.
+//
+
+import UIKit
+
+class TermsConditionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
